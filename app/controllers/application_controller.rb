41252529248e702a6585ebffc201e23a67216957
Joon Hyun <doublejtoh@naver.com>
2017-05-14 13:03:54 +0000

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,23 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+  
+  def authenticate
+    redirect_to :new_user_session if !user_signed_in?
+  end
+  
+  def authenticate_admin
+   # flash[:alert] = '관리자 권한이 없습니다.'
+    redirect_to :root unless current_user.admin
+  end
+  
+end
---
+++ app/controllers/festival_controller.rb
@@ -0,0 +1,39 @@
+class FestivalController < ApplicationController
+    before_action :authenticate, only: [:write]
+    before_action :authenticate_admin, only: [:write]
+    
+    def day1
+    
+        
+        
+    end
+    
+    
+    def day2
+            
+    end
+            
+            
+    def day3
+        
+    end
+    
+    def day4
+    
+    end
+    
+    def index
+        
+    end
+    
+    def write
+        
+    end
+            
+            
+    def eng # 공과대학
+    
+    end
+    
+
+end
